0e4ec14869e7ebfb76058c3e8d614adf81ad1ed2
David Melia <melia.dave@gmail.com>
2023-01-31 12:23:11 +0000

    first stab

--- src/main/java/example/WebClientFunction.java
+++ src/main/java/example/WebClientFunction.java
@@ -1,33 +1,39 @@
 package example;
 
+import io.micrometer.context.ContextSnapshot;
+import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor;
 import java.time.Duration;
 import java.util.Map;
 import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
-@Component(value = "function2")
+@Component(value = "function")
 public class WebClientFunction implements Function<Flux<Map<String, String>>, Flux<String>> {
 
 	private WebClient webClient;
 	
-	  public WebClientFunction(StreamBridge streamBridge, Builder webClientBuilder) {
+	  public WebClientFunction(Builder webClientBuilder) {
 	    this.webClient = webClientBuilder.baseUrl("https://httpbin.org/json").build();
 	  }
 
 	@Override
 	public Flux<String> apply(Flux<Map<String, String>> flux) {
 		return flux.flatMap(response -> {
+          return Mono.deferContextual(contextView -> {
+            try (ContextSnapshot.Scope scope = ContextSnapshot.setThreadLocalsFrom(contextView,
+                    ObservationThreadLocalAccessor.KEY)) {
+		  
 		    log.info("response is {}",response);
 			return webClient.get().retrieve().bodyToMono(Map.class).map(r -> {log.info("response={}",r); return r;} ).thenReturn("OK").delayElement(Duration.ofSeconds(4));
-	
+            }});
 		});
 	}
-
+	
 
 }
--- src/test/java/example/CallFunctionInvoker.java
+++ src/test/java/example/CallFunctionInvoker.java
@@ -2,23 +2,22 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Map;
-
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CallFunctionInvoker {
 
 	@Test
 	public void runFunctionInvoker() throws IOException {
+	    System.setProperty("management.tracing.sampling.probability", "1.0");
+	     System.setProperty("management.zipkin.tracing.endpoint", "http://localhost:9411/api/v2/spans");  
 		FunctionInvoker handler = new FunctionInvoker("function");
 		ObjectMapper om = new ObjectMapper();
 		ByteArrayInputStream is = new ByteArrayInputStream(om.writeValueAsBytes(Map.of()));
