4a91bd2c2e5440d286cd1bdba53d1f325985e77c
David Melia <melia.dave@gmail.com>
2023-02-21 09:33:32 +0000

    using Hooks

--- src/main/java/example/WebClientFunction.java
+++ src/main/java/example/WebClientFunction.java
@@ -1,15 +1,16 @@
 package example;
 
-import io.micrometer.context.ContextSnapshot;
-import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor;
-import java.time.Duration;
+import io.micrometer.observation.ObservationRegistry;
 import java.util.Map;
 import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
+import reactor.core.observability.micrometer.Micrometer;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -18,28 +19,24 @@ public class WebClientFunction implements Function<Flux<Map<String, String>>, Fl
 
   private WebClient webClient;
 
-  // @Autowired
-  // private Tracer tracer;
+  @Autowired
+  private ObservationRegistry observationRegistry;
 
   public WebClientFunction(Builder webClientBuilder) {
+    Hooks.enableAutomaticContextPropagation();
     this.webClient = webClientBuilder.baseUrl("https://httpbin.org/json").build();
   }
 
   @Override
   public Flux<String> apply(Flux<Map<String, String>> flux) {
-    return flux.flatMap(response -> {
-      // tracer.startScopedSpan("dave");
-      return Mono.deferContextual(contextView -> {
-        try (ContextSnapshot.Scope scope = ContextSnapshot.setThreadLocalsFrom(contextView, ObservationThreadLocalAccessor.KEY)) {
+    return flux.flatMap(request -> {
+      log.info("1) this does not have a trace id");
+      return Mono.just(request).doOnNext(r -> log.info("2) this does not have a trace id", request)).then(webClient.get().retrieve().bodyToMono(Map.class).map(r -> {
+        log.info("3) this does have a trace id");
+        return r;
+      }).thenReturn("OK"));
 
-          log.info("response is {}", response);
-          return webClient.get().retrieve().bodyToMono(Map.class).map(r -> {
-            log.info("response={}", r);
-            return r;
-          }).thenReturn("OK").delayElement(Duration.ofSeconds(4));
-        }
-      });
-    });
+    }).tap(Micrometer.observation(observationRegistry));
   }
 
 
