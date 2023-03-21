a9954241069a907bcd4b1444d64a429980865e9c
David Melia <melia.dave@gmail.com>
2023-03-21 09:42:53 +0000

    better

---
+++ src/main/example/DemoApplication.java
@@ -0,0 +1,21 @@
+package example;
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+  
+  @PostConstruct
+  public void init() {
+    Hooks.enableAutomaticContextPropagation();
+  }
+
+}
