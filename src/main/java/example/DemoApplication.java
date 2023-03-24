91f90aa42f62581433c84972f7db0f97277fdea4
David Melia <melia.dave@gmail.com>
2023-03-24 16:26:28 +0000

    fix memory leak

--- src/main/java/example/DemoApplication.java
+++ src/main/java/example/DemoApplication.java
@@ -1,9 +1,16 @@
 package example;
 
 
+import brave.baggage.BaggageField;
+import brave.baggage.CorrelationScopeConfig;
+import brave.baggage.CorrelationScopeCustomizer;
 import jakarta.annotation.PostConstruct;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.tracing.TracingProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
@@ -18,4 +25,15 @@ public class DemoApplication {
     Hooks.enableAutomaticContextPropagation();
   }
 
+  @Bean
+  @Order(1)
+  CorrelationScopeCustomizer myCorrelationFieldsCorrelationScopeCustomizer(TracingProperties tracingProperties) {
+    return (builder) -> {
+      List<String> correlationFields = tracingProperties.getBaggage().getCorrelation().getFields();
+      for (String field : correlationFields) {
+        builder.add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(BaggageField.create(field))
+                .build());
+      }
+    };
+  }
 }
