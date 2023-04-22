7a0aea5f3651e6b5db0b11b0c973c38dcf5dea4e
David Melia <melia.dave@gmail.com>
2023-04-22 09:54:40 +0100

    memory leak 2

--- spamHealthCheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..500}
-do
-   curl http://localhost:8080/actuator/health
-done
--- src/main/java/example/DemoApplication.java
+++ src/main/java/example/DemoApplication.java
@@ -4,6 +4,9 @@ package example;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
@@ -11,6 +14,21 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+  }
+  
+  @Bean
+  public WebFluxConfigurer corsConfigurer() {
+    return new WebFluxConfigurer() {
+      @Override
+      public void addCorsMappings(org.springframework.web.reactive.config.CorsRegistry registry) {
+
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods(HttpMethod.GET.toString(), HttpMethod.POST.toString(), HttpMethod.PUT.toString(), HttpMethod.DELETE.toString(), HttpMethod.OPTIONS.toString(),
+                HttpMethod.PATCH.toString(), HttpMethod.HEAD.toString());
+      }
+
+    };
   }
   
   @PostConstruct
